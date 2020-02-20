89aa7488b9422dc3a04a6be5a0bf4e5df35530a0
YefymChirochkin <55995252+YefymChirochkin@users.noreply.github.com>
2020-02-20 16:52:00 +0200

    Correct the error (incorrectly declared variable)

--- src/ua/lviv/iot/firstLab/WaterPumpOrganizer.java
+++ src/ua/lviv/iot/firstLab/WaterPumpOrganizer.java
@@ -29,7 +29,7 @@ public class WaterPumpOrganizer {
 		}
 
 		for (WaterPump currentCount : arrayOfDetails) {
-			System.out.println(currentCountOfDetails);
+			System.out.println(currentCount);
 		}
 
 	}
